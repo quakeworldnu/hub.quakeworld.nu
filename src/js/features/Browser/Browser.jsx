1b495d1440cb26dc265ce1a24c9694ba65cfa4a4
Viktor Persson <viktor.persson@arcsin.se>
2021-05-05 15:11:51 +0200

    Apply bps styles.

--- src/js/features/Browser/Browser.jsx
+++ src/js/features/Browser/Browser.jsx
@@ -1,10 +1,68 @@
-import React from "react";
-import { connect } from "react-redux";
+import React from 'react';
+import { connect } from 'react-redux';
+
+const getClassNames = server => {
+  const classNames = [];
+
+  classNames.push('item');
+
+  return classNames;
+};
+
+const Server = (props) => {
+  const { server } = props;
+  const classNamesStr = getClassNames(server).join(' ');
+  const gameMode = server.Description.split(', ')[0];
+
+  const players = server.Players.filter(p => !p.Spec);
+  const spectators = server.Players.filter(p => p.Spec);
+
+  return (
+    <div className={classNamesStr}>
+
+      <div className="header">
+        <div className="mapname">{server.Settings.map}</div>
+        <div className="mode">{gameMode}</div>
+      </div>
+
+      <div className="players">
+        {players.map(player => (
+          <><span className="playercolor" /> {player.Name}<br /></>
+        ))}
+      </div>
+
+      <div className="specs">
+        {
+          spectators.map(spec => (
+            <><span className="speccolor">spectator</span> {spec.Name}<br /></>
+          ))
+        }
+      </div>
+
+      <div className="bottom">
+        {server.Address}
+        <div className="serverdetails">
+          <a href="#">QTV&nbsp;&nbsp;</a>
+          <a href="#">Spectate&nbsp;&nbsp;</a>
+          <a href="#">Join</a>
+        </div>
+      </div>
+
+    </div>
+  );
+};
 
 const Browser = (props) => {
   return (
-    <div>
-      <pre>{JSON.stringify(props, null, 2)}</pre>
+    <div className="masonry">
+      {props.servers && props.servers.entries.map((entry, index) => {
+          return (
+            <div className="tile is-parent">
+              <Server key={index} server={entry} />
+            </div>
+          );
+        },
+      )}
     </div>
   );
 };
