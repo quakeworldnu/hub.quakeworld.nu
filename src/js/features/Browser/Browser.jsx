4daea85afe838826d69c363136a714b0e7f523d0
Viktor Persson <viktor.persson@arcsin.se>
2021-05-05 16:12:22 +0200

    Update browser.

--- src/js/features/Browser/Browser.jsx
+++ src/js/features/Browser/Browser.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import browserSlice from './slice';
+import {shuffleArray} from './../../common/util';
 
-const getClassNames = server => {
+const getClassNames = (server) => {
   const classNames = [];
 
   classNames.push('item');
@@ -13,30 +15,32 @@ const Server = (props) => {
   const { server } = props;
   const classNamesStr = getClassNames(server).join(' ');
   const gameMode = server.Description.split(', ')[0];
-
-  const players = server.Players.filter(p => !p.Spec);
-  const spectators = server.Players.filter(p => p.Spec);
+  const players = server.Players.filter((p) => !p.Spec);
+  const spectators = server.Players.filter((p) => p.Spec);
 
   return (
     <div className={classNamesStr}>
-
       <div className="header">
         <div className="mapname">{server.Settings.map}</div>
         <div className="mode">{gameMode}</div>
       </div>
 
       <div className="players">
-        {players.map(player => (
-          <><span className="playercolor" /> {player.Name}<br /></>
+        {players.map((player, index) => (
+          <React.Fragment key={index}>
+            <span className="playercolor" /> {player.Name}
+            <br />
+          </React.Fragment>
         ))}
       </div>
 
       <div className="specs">
-        {
-          spectators.map(spec => (
-            <><span className="speccolor">spectator</span> {spec.Name}<br /></>
-          ))
-        }
+        {spectators.map((spec, index) => (
+          <React.Fragment key={index}>
+            <span className="speccolor">spectator</span> {spec.Name}
+            <br />
+          </React.Fragment>
+        ))}
       </div>
 
       <div className="bottom">
@@ -47,28 +51,67 @@ const Server = (props) => {
           <a href="#">Join</a>
         </div>
       </div>
-
     </div>
   );
 };
 
-const Browser = (props) => {
-  return (
-    <div className="masonry">
-      {props.servers && props.servers.entries.map((entry, index) => {
+const serverEntriesProvider = {
+  get: () => {
+    const url = '/data/busy.json';
+    const options = {
+      method: 'GET',
+      mode: 'cors',
+      cache: 'no-cache',
+      redirect: 'follow',
+
+    };
+    return fetch(url, options).then(response => response.json()).then(data => {
+      shuffleArray(data);
+      return data;
+    });
+  },
+};
+
+class Browser extends React.Component {
+  componentDidMount() {
+    const refreshInterval = 20000;
+
+    const fetchAndUpdateEntries = () => {
+      return serverEntriesProvider.get().
+        then(entries => this.props.updateEntries({ entries }));
+    };
+
+    this.fetchEntriesInterval = setInterval(fetchAndUpdateEntries, refreshInterval);
+    fetchAndUpdateEntries();
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.fetchEntriesInterval);
+  }
+
+  render() {
+    console.log('Browser::render()');
+
+    return (
+      <div className="masonry">
+        {this.props.servers &&
+        this.props.servers.entries.map((entry, index) => {
           return (
-            <div className="tile is-parent">
-              <Server key={index} server={entry} />
+            <div key={index} className="tile is-parent">
+              <Server server={entry} />
             </div>
           );
-        },
-      )}
-    </div>
-  );
-};
+        })}
+      </div>
+    );
+  }
+}
 
 const mapStateToProps = (state) => state;
+const mapDispatchToProps = {
+  updateEntries: browserSlice.actions.updateEntries,
+};
 
-const BrowserComponent = connect(mapStateToProps)(Browser);
+const BrowserComponent = connect(mapStateToProps, mapDispatchToProps)(Browser);
 
 export default BrowserComponent;
