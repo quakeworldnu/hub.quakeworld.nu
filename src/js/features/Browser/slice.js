383a716a323874be3f3457cd1917a7f19d708898
Viktor Persson <viktor.persson@arcsin.se>
2021-05-05 16:11:31 +0200

    Update browser slice.

--- src/js/features/Browser/slice.js
+++ src/js/features/Browser/slice.js
@@ -1,7 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-import servers from "../../data/busy.json";
-
 const getInitialState = () => ({
   ui: {
     favorites: {
@@ -10,34 +8,16 @@ const getInitialState = () => ({
     },
     filters: {},
   },
-  entries: servers,
+  entries: [],
 });
 
 export default createSlice({
   name: "form",
   initialState: getInitialState(),
   reducers: {
-    /*
-    add: (state) => {
-
-      const lastEntry = Object.values(state).pop();
-      const newId = parseInt(lastEntry.id) + 1;
-
-      state[newId] = {
-        ...deepCopy(lastEntry),
-        id: newId,
-      };
+    updateEntries: (state, action) => {
+      const { entries } = action.payload;
+      state.entries = entries;
     },
-    update: (state, action) => {
-      const { id, values } = action.payload;
-
-      state[id].values = values;
-      state[id].result = {};
-    },
-    remove: (state, action) => {
-      const { id } = action.payload;
-      delete state[id];
-    },
-     */
   },
 });
