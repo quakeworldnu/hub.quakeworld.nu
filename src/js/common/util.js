321c5c4c0728c77530009147388a06416fe1e997
Viktor Persson <viktor.persson@arcsin.se>
2021-05-05 16:11:22 +0200

    Add util method.

--- src/js/common/util.js
+++ src/js/common/util.js
@@ -1,1 +1,16 @@
 export const deepCopy = (obj) => JSON.parse(JSON.stringify(obj));
+
+export const shuffleArray = (array) => {
+  let currentIndex = array.length, temporaryValue, randomIndex;
+
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+};
