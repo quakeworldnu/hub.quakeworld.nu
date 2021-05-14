a7397502dbd6e81293e9acacce0c60d15ddfc415
Viktor Persson <viktor.persson@arcsin.se>
2021-05-14 22:47:54 +0200

    Refactor styles.

--- src/js/features/Browser/Browser.jsx
+++ src/js/features/Browser/Browser.jsx
@@ -99,7 +99,7 @@ const Server = (props) => {
                   <tr key={index}>
                     <td className="app-text-small">{player.Ping}</td>
                     <td
-                      className={`app-text-small has-text-weight-bold color-${player.Colors[0]}-${player.Colors[1]}`}
+                      className={`app-text-small has-text-weight-bold app-color-${player.Colors[0]}-${player.Colors[1]}`}
                     >
                       {player.Frags}
                     </td>
