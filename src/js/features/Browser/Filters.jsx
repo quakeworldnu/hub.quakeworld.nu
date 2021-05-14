2c95eefc1788e8b3a1f516dedc09e7f7267ee094
Viktor Persson <viktor.persson@arcsin.se>
2021-05-14 20:19:15 +0200

    Add more filters.

--- src/js/features/Browser/Filters.jsx
+++ src/js/features/Browser/Filters.jsx
@@ -14,13 +14,42 @@ const FilterForm = (props) => {
       validateOnChange
     >
       <form>
-        <div className="field">
-          <Field
-            type="search"
-            className="input"
-            name="keyword"
-            placeholder="Find player"
-          />
+        <div className="columns">
+          <div className="field column is-narrow">
+            <label className="label" htmlFor="region">
+              Region
+            </label>
+            <div className="control">
+              <div className="select">
+                <select name="" id="region">
+                  <option value="">(any region)</option>
+                  <option value="eu">Europe</option>
+                  <option value="ru">Russia</option>
+                  <option value="na">North America</option>
+                  <option value="sa">South America</option>
+                  <option value="af">Africa</option>
+                  <option value="as">Asia</option>
+                </select>
+              </div>
+            </div>
+          </div>
+          <div className="field column is-narrow">
+            <label className="label" htmlFor="keyword">
+              Player
+            </label>
+            <Field
+              type="search"
+              className="input"
+              id="keyword"
+              name="keyword"
+            />
+          </div>
+          <div className="field column is-narrow">
+            <label className="label" htmlFor="keyword">
+              Misc
+            </label>
+            <div>[ ] Hide servers with no players</div>
+          </div>
         </div>
       </form>
     </Formik>
