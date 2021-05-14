7b250ab69cabb1fd76555edde3ae7a825584b41f
Viktor Persson <viktor.persson@arcsin.se>
2021-05-14 22:24:10 +0200

    Fix search input.

--- src/js/features/Browser/Filters.jsx
+++ src/js/features/Browser/Filters.jsx
@@ -14,13 +14,29 @@ const FilterForm = (props) => {
       validateOnChange
     >
       <form className="form m-0">
-        <Field
-          type="search"
-          className="input"
-          id="keyword"
-          name="keyword"
-          placeholder="Search"
-        />
+        <div className="field has-addons">
+          <p className="control">
+            <a className="button is-static px-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="24px"
+                viewBox="0 0 24 24"
+                width="24px"
+                fill="#666666"
+              >
+                <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z" />
+              </svg>
+            </a>
+          </p>
+          <div className="control is-expanded">
+            <Field
+              type="search"
+              className="input"
+              id="keyword"
+              name="keyword"
+            />
+          </div>
+        </div>
       </form>
     </Formik>
   );
@@ -33,14 +49,12 @@ const Filters = (props) => {
 
   return <FilterForm values={formValues} onValidate={handleValidate} />;
 };
-
 const mapStateToProps = (state) => ({
   formValues: state.servers.ui.filters,
 });
 const mapDispatchToProps = {
   handleFormChange: slice.actions.updateFilters,
 };
-
 const FilterFormComponent = connect(
   mapStateToProps,
   mapDispatchToProps
