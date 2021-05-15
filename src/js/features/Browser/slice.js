2b94ed54f025814f2deebb32110364e149efc5d1
Viktor Persson <viktor.persson@arcsin.se>
2021-05-15 16:32:11 +0200

    Remove obsolete state var.

--- src/js/features/Browser/slice.js
+++ src/js/features/Browser/slice.js
@@ -4,7 +4,6 @@ import { metaByServer, compareServers } from "../../common/util";
 const getInitialState = () => ({
   ui: {
     favorites: {
-      players: [],
       servers: [],
     },
     filters: {
