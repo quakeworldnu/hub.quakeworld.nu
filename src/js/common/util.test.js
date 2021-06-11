57f3b078d30cf4210f49769ad37006af2905c4d4
Viktor Persson <viktor.persson@arcsin.se>
2021-06-11 13:11:43 +0200

    Add tests.

--- src/js/common/util.test.js
+++ src/js/common/util.test.js
@@ -1,9 +1,35 @@
-import { deepCopy } from "./util";
+import { deepCopy, serverAddressTitleByServer } from "./util";
 
 describe("util", () => {
   test("deepCopy", () => {
     const original = { name: "John" };
     const copy = deepCopy(original);
     expect(original === copy).toBeFalsy();
+  });
+
+  test("serverTitleByServer", () => {
+    expect(
+      serverAddressTitleByServer({
+        IpAddress: "212.42.38.88",
+        Address: "quake.com:28502",
+        Title: "quake server #1",
+      })
+    ).toEqual("quake.com:28502");
+
+    expect(
+      serverAddressTitleByServer({
+        IpAddress: "212.42.38.88",
+        Address: "188.166.0.137:28502",
+        Title: "quake.com:28502",
+      })
+    ).toEqual("quake.com:28502");
+
+    expect(
+      serverAddressTitleByServer({
+        IpAddress: "212.42.38.88",
+        Address: "188.166.0.137:28502",
+        Title: "quake.com",
+      })
+    ).toEqual("quake.com:28502 (212.42.38.88)");
   });
 });
