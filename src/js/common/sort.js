f6671b4f1a704d52349585a8ce4f698ddddca144
Viktor Persson <viktor.persson@arcsin.se>
2022-05-18 15:54:12 +0200

    Update server sorting, prevent flickering of servers with similar properties.

--- src/js/common/sort.js
+++ src/js/common/sort.js
@@ -31,6 +31,20 @@ export const compareServers = (a, b) => {
     return 1;
   }
 
+  // player slots
+  if (a.PlayerSlots.Free < b.PlayerSlots.Free) {
+    return -1;
+  } else if (a.PlayerSlots.Free > b.PlayerSlots.Free) {
+    return 1;
+  }
+
+  // Address
+  if (a.Address < b.Address) {
+    return -1;
+  } else if (a.Address > b.Address) {
+    return 1;
+  }
+
   return 0;
 };
 
