2cce32c43e977d4f0bddfb69ced841bc05ed29f8
Viktor Persson <viktor.persson@arcsin.se>
2022-05-24 14:58:50 +0200

    Animate browser tiles.

--- src/js/features/Browser/Browser.jsx
+++ src/js/features/Browser/Browser.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
+import { useAutoAnimate } from "@formkit/auto-animate/react";
 import browserSlice from "./slice";
 import Filters from "./Filters";
 import Overview from "./Overview";
@@ -25,15 +26,16 @@ const BrowserHeader = () => (
 
 const BrowserTiles = (props) => {
   const { servers } = props;
+  const [parent] = useAutoAnimate();
 
   if (0 === servers.length) {
     return null;
   }
 
   return (
-    <div className="app-grid">
-      {servers.map((entry, index) => {
-        return <Server key={index} server={entry} />;
+    <div className="app-grid" ref={parent}>
+      {servers.map((server, index) => {
+        return <Server key={index} server={server} />;
       })}
     </div>
   );
