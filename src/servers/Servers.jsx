3f0793d704f216056c849a689a0fdaae3022329a
Viktor Persson <vikpe@users.noreply.github.com>
2023-03-08 00:43:30 +0100

    Disable auto animate

--- src/servers/Servers.jsx
+++ src/servers/Servers.jsx
@@ -1,17 +1,16 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { useAutoAnimate } from "@formkit/auto-animate/react";
+//import { useAutoAnimate } from "@formkit/auto-animate/react";
 import { Server } from "./Server.jsx";
 import { selectAllServerAddresses } from "@/services/hub/servers";
 
 export default function Servers() {
-  const [parent] = useAutoAnimate();
   const serverAddresses = useSelector(selectAllServerAddresses);
   const hasServers = serverAddresses.length > 0;
 
   return (
     <div className="my-6">
-      <div className="grid grid-cols-servers gap-4 sm:gap-8" ref={parent}>
+      <div className="grid grid-cols-servers gap-4 sm:gap-8">
         {hasServers &&
           serverAddresses.map((address) => (
             <Server key={address} address={address} />
