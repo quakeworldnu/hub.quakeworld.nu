7c82677e4cbcb22049c8c6f6ce5a91ba2348345d
Viktor Persson <viktor.persson@arcsin.se>
2023-03-08 17:09:22 +0100

    Refactor imports for scoreboard index.

--- src/pages/scoreboard/index.jsx
+++ src/pages/scoreboard/index.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import store from "@/store.js";
-import App from "./App.jsx";
+import store from "@/store";
+import App from "./App";
 import "@/styles/index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
