e23f87ef0af89e032ea33b7adcd194e6b9c6fe75
Viktor Persson <viktor.persson@arcsin.se>
2023-05-08 17:21:29 +0200

    Update footer links.

--- src/site/Footer.jsx
+++ src/site/Footer.jsx
@@ -21,7 +21,7 @@ export const SiteFooter = () => {
           <a href="https://tools.quake.world/recent-demos/">Recent demos</a>
         </div>
         <div className="sm:text-right">
-          <a href="https://github.com/quakeworldnu/hub.quakeworld.nu">
+          <a href="https://github.com/quakeworldnu/hub.quakeworld.nu" className="app-link">
             Source on GitHub
           </a>
         </div>
