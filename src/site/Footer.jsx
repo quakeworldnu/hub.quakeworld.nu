82d470915f5812dc666310aa9c0081717869a8f8
Viktor Persson <viktor.persson@arcsin.se>
2023-06-23 19:10:12 +0200

    Update footer links.

--- src/site/Footer.jsx
+++ src/site/Footer.jsx
@@ -6,28 +6,29 @@ export const SiteFooter = () => {
     <div id="app-footer" className="text-sm">
       <HorizontalSeparator />
 
-      <div className="footer my-6 sm:columns-2 text-gray-600">
-        <div className="mb-3 space-x-1 app-links">
-          <a href="https://www.quakeworld.nu/">QuakeWorld.nu</a>
-          {delimiter}
-          <a href="https://discord.quake.world/">Discord</a>
-          {delimiter}
-          <a href="https://www.twitch.tv/quakeworld">Twitch</a>
-          {delimiter}
-          <a href="https://www.quakeworld.nu/wiki/Overview">Wiki</a>
-          {delimiter}
-          <a href="https://tools.quake.world/servers/">Servers</a>
-          {delimiter}
-          <a href="https://tools.quake.world/recent-demos/">Recent demos</a>
-        </div>
-        <div className="sm:text-right">
-          <a
-            href="https://github.com/quakeworldnu/hub.quakeworld.nu"
-            className="app-link"
-          >
-            Source on GitHub
-          </a>
-        </div>
+      <div className="footer my-6 sm text-gray-600 leading-loose mb-3 space-x-1 app-links">
+        <a href="https://quake.world/">Quake.World</a>
+        {delimiter}
+        <a href="https://www.quakeworld.nu/">QuakeWorld.nu</a>
+        {delimiter}
+        <a href="https://discord.quake.world/">Discord</a>
+        {delimiter}
+        <a href="https://www.twitch.tv/quakeworld">Twitch</a>
+        {delimiter}
+        <a href="https://www.quakeworld.nu/wiki/Overview">Wiki</a>
+        {delimiter}
+        <a href="https://tools.quake.world/servers/">Servers</a>
+        {delimiter}
+        <a href="https://tools.quake.world/recent-demos/">Recent demos</a>
+        {delimiter}
+        <a href="https://tools.quake.world/">Tools</a>
+        {delimiter}
+        <a
+          href="https://github.com/quakeworldnu/hub.quakeworld.nu"
+          className="grayscale"
+        >
+          Source on GitHub
+        </a>
       </div>
     </div>
   );
