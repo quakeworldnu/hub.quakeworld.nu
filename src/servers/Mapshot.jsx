8f16b46e549f9cc49d44c79eff07943a6949ff03
Viktor Persson <viktor.persson@arcsin.se>
2023-07-08 20:48:56 +0200

    Refactor mapshot.

---
+++ src/servers/Mapshot.jsx
@@ -0,0 +1,11 @@
+export function Mapshot({ map, children }) {
+  const backgroundImage = `url(https://raw.githubusercontent.com/vikpe/qw-mapshots/main/${map}.jpg)`;
+
+  return (
+    <div className="h-full bg-cover bg-center bg-[url(/assets/img/default_mapshot.jpg)]">
+      <div className="h-full bg-cover bg-center" style={{ backgroundImage }}>
+        <div className="h-full bg-gray-700/20">{children}</div>
+      </div>
+    </div>
+  );
+}
