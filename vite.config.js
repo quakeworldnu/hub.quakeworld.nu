efd58d104a20f5b1275b934bff74982dc57b195f
Viktor Persson <viktor.persson@arcsin.se>
2023-07-10 13:38:13 +0200

    Add demos entrypoint.

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
+        demos: resolve(__dirname, 'demos/index.html'),
         players: resolve(__dirname, 'players/index.html'),
         scoreboard: resolve(__dirname, 'scoreboard/index.html'),
       }
