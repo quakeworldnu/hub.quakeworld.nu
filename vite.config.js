ae38aa876e3248d137dc5adfcae96b948d8313b3
Viktor Persson <viktor.persson@arcsin.se>
2023-07-26 12:47:34 +0200

    Add lightningcss.

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
+    cssMinify: 'lightningcss',
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
