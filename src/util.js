a60035be75735f41718b57ac4ed554ddbcce0309
Viktor Persson <viktor.persson@arcsin.se>
2023-08-21 21:19:58 +0200

    Add methods to interact with localStorage.

---
+++ src/util.js
@@ -0,0 +1,21 @@
+export function localStorageSet(key, value) {
+  localStorage.setItem(key, JSON.stringify(value));
+}
+
+export function localStorageGet(key, defaultValue) {
+  const settings = localStorage.getItem(key);
+  return settings ? JSON.parse(settings) : defaultValue;
+}
+
+export function slugify(str) {
+  return str
+    .toLowerCase()
+    .trim()
+    .replace(/[^\w\s-]/g, "")
+    .replace(/[\s_-]+/g, "-")
+    .replace(/^-+|-+$/g, "");
+}
+
+export function toId(str) {
+  return slugify(str).replace(/-/g, "_");
+}
