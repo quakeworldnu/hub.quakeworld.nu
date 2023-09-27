c81a7af91922f9365a637a6db3932e387db3e3ea
Viktor Persson <viktor.persson@arcsin.se>
2023-09-27 15:09:19 +0200

    Reformat code.

--- src/pages/demo_player/nano/components/progressbar.jsx
+++ src/pages/demo_player/nano/components/progressbar.jsx
@@ -1,16 +1,19 @@
-import * as React from "react"
-import * as progressStyle from "./progressbar.module.scss"
+import * as React from "react";
+import * as progressStyle from "./progressbar.module.scss";
 
-const Progressbar = props => {
-  const { completed } = props
+const Progressbar = (props) => {
+  const { completed } = props;
 
   return (
     <div className={progressStyle.progressContainer}>
       <div className={`${progressStyle.progress2}`}>
-        <div className={progressStyle.progressbar2} style={{ width: `${completed}%` }} />
+        <div
+          className={progressStyle.progressbar2}
+          style={{ width: `${completed}%` }}
+        />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Progressbar
+export default Progressbar;
