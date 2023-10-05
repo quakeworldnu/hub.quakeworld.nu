ebd07da065ca4c0c220fb37f3d02b0d73da12ba4
Viktor Persson <viktor.persson@arcsin.se>
2023-10-05 21:58:36 +0200

    Refactor controls.

---
+++ src/pages/demo_player/fte/GroupControls.jsx
@@ -0,0 +1,74 @@
+import {
+  useFteController,
+  useFteEventBySource,
+} from "@qwhub/pages/demo_player/fte/hooks";
+import { useUser } from "@qwhub/pages/demo_player/services/convex/hooks";
+import { useEffect, useState } from "react";
+import { Debug } from "@qwhub/pages/demo_player/fte/Debug";
+
+export const GroupControls = () => {
+  const fte = useFteController();
+  const { user, group, playback, createPlayback, updatePlayback } = useUser();
+  const [isSynced, setIsSynced] = useState(false);
+
+  // initial group playback
+  useEffect(() => {
+    if (fte && playback === null) {
+      createPlayback({
+        demo_jump: fte.getDemoElapsedTime(),
+        demo_setspeed: fte.demo_setspeed(),
+        cl_autotrack: fte.cl_autotrack(),
+        track: fte.getTrackUserid(),
+      });
+    }
+  }, [fte, playback]);
+
+  // follow group playback
+  useEffect(() => {
+    console.log(
+      fte === undefined,
+      typeof playback,
+      isSynced,
+      playback && playback.updateUserId === user._id,
+    );
+
+    if (!fte || !playback || (isSynced && playback.updateUserId === user._id)) {
+      return;
+    }
+
+    fte.applyPlayback(playback);
+
+    if (!isSynced) {
+      setIsSynced(true);
+    }
+  }, [fte, playback, isSynced]);
+
+  // update group playback
+  useFteEventBySource("demo_jump", "user", (e) => {
+    updatePlayback({ demo_jump: e.detail.value });
+  });
+  useFteEventBySource("demo_setspeed", "user", (e) => {
+    updatePlayback({ demo_setspeed: e.detail.value });
+  });
+  useFteEventBySource("cl_autotrack", "user", (e) => {
+    updatePlayback({ cl_autotrack: e.detail.value });
+  });
+  useFteEventBySource("track", "user", (e) => {
+    updatePlayback({ track: e.detail.value });
+  });
+
+  return (
+    <div>
+      <div className="flex space-between">
+        <Debug
+          value={{
+            fte: typeof fte,
+            playback: typeof playback,
+          }}
+        />
+        <Debug value={group} />
+        <Debug value={playback} />
+      </div>
+    </div>
+  );
+};
