5c73940942089618b690d24bc605a1bf1a6cfdcf
Viktor Persson <viktor.persson@arcsin.se>
2023-10-12 18:19:23 +0200

    Refactor supabase.

--- src/pages/demo_player/services/supabase/supabase.ts
+++ src/pages/demo_player/services/supabase/supabase.ts
@@ -2,9 +2,15 @@ import { createClient } from "@supabase/supabase-js";
 
 // import { Database  } from "./database.types.ts"
 
+const client = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY,
+);
+
 export function getClient() {
-  return createClient(
-    import.meta.env.SUPABASE_URL,
-    import.meta.env.SUPABASE_ANON_KEY,
-  );
+  return client;
+}
+
+export async function getDemo(id: number) {
+  return client.from("demos").select("*").eq("id", id).limit(1).single();
 }
