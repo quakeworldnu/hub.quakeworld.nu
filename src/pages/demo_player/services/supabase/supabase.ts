1432534f11a9b7a75e82b89ad9a4af1bda763aa8
Viktor Persson <viktor.persson@arcsin.se>
2023-10-13 16:26:38 +0200

    Refactor supabase.

--- src/pages/demo_player/services/supabase/supabase.ts
+++ src/pages/demo_player/services/supabase/supabase.ts
@@ -1,16 +1,16 @@
 import { createClient } from "@supabase/supabase-js";
 
-// import { Database  } from "./database.types.ts"
+import { Database } from "./database.types.ts";
 
-const client = createClient(
+const supabase = createClient<Database>(
   import.meta.env.VITE_SUPABASE_URL,
   import.meta.env.VITE_SUPABASE_ANON_KEY,
 );
 
 export function getClient() {
-  return client;
+  return supabase;
 }
 
 export async function getDemo(id: number) {
-  return client.from("demos").select("*").eq("id", id).limit(1).single();
+  return supabase.from("demos").select("*").eq("id", id).limit(1).single();
 }
---
+++ src/pages/demo_player/services/supabase/supabase.types.ts
@@ -0,0 +1,31 @@
+import { Database } from "./database.types.ts";
+import { PostgrestError } from "@supabase/supabase-js";
+
+export type Tables<T extends keyof Database["public"]["Tables"]> =
+  Database["public"]["Tables"][T]["Row"];
+
+export type DbResult<T> = T extends PromiseLike<infer U> ? U : never;
+export type DbResultOk<T> = T extends PromiseLike<{ data: infer U }>
+  ? Exclude<U, null>
+  : never;
+export type DbResultErr = PostgrestError;
+
+export type Demo = Tables<"demos">;
+
+export type Player = {
+  name: string;
+  team: string;
+  top_color: number;
+  bottom_color: number;
+  frags: number;
+  teamkills: number;
+  deaths: number;
+  suicides: number;
+  ping: number;
+};
+
+export type Team = {
+  name: string;
+  frags: number;
+  players: Player[];
+};
