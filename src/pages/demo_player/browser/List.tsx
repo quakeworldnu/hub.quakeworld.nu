730e7604a9d35e767a3aee3812eee212f34d442b
Viktor Persson <viktor.persson@arcsin.se>
2023-10-19 17:21:52 +0200

    Update demo list.

--- src/pages/demo_player/browser/List.tsx
+++ src/pages/demo_player/browser/List.tsx
@@ -6,7 +6,7 @@ export const List = ({ demos }: { demos: Demo[] | null }) => {
     <table className="my-6 text-left text-sm">
       <thead>
         <tr>
-          <th className="p-2">Timestamp</th>
+          <th className="p-2 min-w-[120px]">Timestamp</th>
           <th className="p-2">Mode</th>
           <th className="p-2">Map</th>
           <th className="p-2">Title</th>
@@ -26,12 +26,12 @@ function demoDownloadUrl(s3Key: string) {
 
 const ListItem = ({ demo }: { demo: Demo }) => {
   return (
-    <tr className="odd:bg-white/5">
+    <tr className="odd:bg-[#1a1a2a] hover:bg-white/10">
       <td className="p-2 text-slate-400 text-xs">
         <Timestamp timestamp={demo.timestamp} />
       </td>
       <td className="p-2 text-slate-400 text-right">{demo.mode}</td>
-      <td className="p-2">{demo.map}</td>
+      <td className="p-2 text-slate-400">{demo.map}</td>
       <td className="p-2">{demo.title}</td>
       <td className="p-2 space-x-3">
         <a href={`/demo_player/?demoId=${demo.id}`} className="text-blue-500">
