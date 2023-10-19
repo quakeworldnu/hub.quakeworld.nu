3aa3af60e245036ac119c8bee0433ffaec7806c1
Viktor Persson <viktor.persson@arcsin.se>
2023-10-19 16:00:19 +0200

    Update supabase functions.

--- src/pages/demo_player/services/supabase/supabase.ts
+++ src/pages/demo_player/services/supabase/supabase.ts
@@ -1,16 +1,43 @@
-import { createClient } from "@supabase/supabase-js";
+import { createClient, SupabaseClient } from "@supabase/supabase-js";
 
 import { Database } from "./database.types.ts";
+
+import { DemoBrowserSettings } from "../../browser/types.ts";
 
 const supabase = createClient<Database>(
   import.meta.env.VITE_SUPABASE_URL,
   import.meta.env.VITE_SUPABASE_ANON_KEY,
 );
 
-export function getClient() {
+export function getClient(): SupabaseClient {
   return supabase;
 }
 
 export async function getDemo(id: number) {
   return supabase.from("demos").select("*").eq("id", id).limit(1).single();
+}
+
+function queryToFullTextSearchString(query: string = ""): string {
+  const q = query.trim();
+
+  if (!q) {
+    return "";
+  }
+
+  return q
+    .split(" ")
+    .map((p) => `'${p}'`)
+    .join(" & ");
+}
+
+export async function searchDemos({ query }: DemoBrowserSettings) {
+  let qb = supabase.from("demos").select("*");
+
+  const fts = queryToFullTextSearchString(query);
+
+  if (fts) {
+    qb = qb.textSearch("fts", fts);
+  }
+
+  return qb.order("timestamp", { ascending: false }).limit(60);
 }
