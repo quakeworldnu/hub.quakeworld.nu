2cf3dc8a8c35a4583ab1856364522237a9ee3a83
Viktor Persson <viktor.persson@arcsin.se>
2023-10-22 22:30:14 +0200

    Remove obsolete hook.

--- src/pages/demo_player/hooks.ts
+++ src/pages/demo_player/hooks.ts
@@ -1,16 +1,7 @@
 import { useCounter, useInterval } from "usehooks-ts";
-import { useState } from "react";
 
 export function useUpdateInterval(delay: number | null) {
   const { count, increment } = useCounter(0);
   useInterval(increment, delay);
   return count;
-}
-
-export function useResultByInterval(getValue: () => number, delay: number) {
-  const [value, setValue] = useState(getValue());
-  useInterval(() => {
-    setValue(getValue());
-  }, delay);
-  return value;
 }
