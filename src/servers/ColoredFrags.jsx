4679e496897f579868deebd298592d29069416b0
Viktor Persson <viktor.persson@arcsin.se>
2023-10-26 20:01:28 +0200

    Update colored frags.

--- src/servers/ColoredFrags.jsx
+++ src/servers/ColoredFrags.jsx
@@ -1,14 +1,14 @@
 import { memo } from "react";
 
 export const ColoredFrags = memo((props) => {
-  const { tag = "div", frags, colors } = props;
+  const { tag = "div", frags = "", colors = [0, 0] } = props;
   const TagName = `${tag}`;
 
   return (
     <TagName
-      className={`font-bold text-center qw-bgcolor-${colors[0]}-${colors[1]}`}
+      className={`w-full h-full font-bold text-center qw-bgcolor-${colors[0]}-${colors[1]}`}
     >
-      {frags}
+      <span className="sc-frags">{frags}</span>
     </TagName>
   );
 });
