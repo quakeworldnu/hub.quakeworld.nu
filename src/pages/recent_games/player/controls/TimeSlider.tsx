7aafc56ab587262ba18e57d9d793df02a2611214
Viktor Persson <viktor.persson@arcsin.se>
2023-10-31 17:56:10 +0100

    Update time slider style.

--- src/pages/recent_games/player/controls/TimeSlider.tsx
+++ src/pages/recent_games/player/controls/TimeSlider.tsx
@@ -64,7 +64,7 @@ const SliderRoot = ({ max }: { max: number }) => {
 
   return (
     <Slider.Root
-      className="flex items-center select-none touch-none w-full h-8 group cursor-pointer"
+      className="relative flex items-center select-none touch-none w-full h-8 group cursor-pointer"
       value={[fte.getDemoElapsedTime()]}
       onValueChange={handleValueChange}
       min={0}
@@ -74,7 +74,7 @@ const SliderRoot = ({ max }: { max: number }) => {
       <Slider.Track className="relative grow h-1 transition-size group-hover:h-2 bg-gray-500">
         <Slider.Range className="absolute h-full bg-purple-700 group-hover:bg-purple-600" />
       </Slider.Track>
-      <Slider.Thumb className="block w-1 h-2 bg-purple-500 transition-size focus:outline-none pointer-events-none group-hover:bg-white group-hover:w-1 group-hover:h-5" />
+      <Slider.Thumb className="block w-1 h-2 bg-purple-500 transition-size focus:outline-none pointer-events-none group-hover:bg-white group-hover:h-5" />
     </Slider.Root>
   );
 };
