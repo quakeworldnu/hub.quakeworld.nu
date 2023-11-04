f244af895cd9d9b35bbadbf374859fb475835a2d
Viktor Persson <viktor.persson@arcsin.se>
2023-11-04 21:02:42 +0100

    Refactor switch.

--- src/pages/recent_games/ui/Switch.tsx
+++ src/pages/recent_games/ui/Switch.tsx
@@ -9,39 +9,37 @@ type Props = {
 
 export const Switch = ({ label, enabled, onClick }: Props) => {
   return (
-    <form>
-      <div className="flex items-center cursor-pointer">
-        <label
-          className={classNames("pr-2 cursor-pointer select-none", {
-            "text-gray-300": !enabled,
-            "font-bold": enabled,
-          })}
-          htmlFor={label}
-        >
-          {label}
-        </label>
-        <RadixSwitch.Root
-          onClick={() => onClick()}
+    <div className="flex items-center cursor-pointer">
+      <label
+        className={classNames("pr-2 cursor-pointer select-none", {
+          "text-gray-300": !enabled,
+          "font-bold": enabled,
+        })}
+        htmlFor={label}
+      >
+        {label}
+      </label>
+      <RadixSwitch.Root
+        onClick={() => onClick()}
+        className={classNames(
+          "w-10 h-5 rounded-full relative focus:shadow-black outline-none",
+          {
+            "bg-blue-400": enabled,
+            "bg-gray-500": !enabled,
+          },
+        )}
+        id={label}
+      >
+        <RadixSwitch.Thumb
           className={classNames(
-            "w-10 h-5 rounded-full relative focus:shadow-black outline-none",
+            "block w-4 h-4 rounded-full transition-transform duration-100 translate-x-0.5 will-change-transform",
             {
-              "bg-blue-400": enabled,
-              "bg-gray-500": !enabled,
+              "translate-x-[22px] bg-blue-800": enabled,
+              "bg-gray-700": !enabled,
             },
           )}
-          id={label}
-        >
-          <RadixSwitch.Thumb
-            className={classNames(
-              "block w-4 h-4 rounded-full transition-transform duration-100 translate-x-0.5 will-change-transform",
-              {
-                "translate-x-[22px] bg-blue-800": enabled,
-                "bg-gray-700": !enabled,
-              },
-            )}
-          />
-        </RadixSwitch.Root>
-      </div>
-    </form>
+        />
+      </RadixSwitch.Root>
+    </div>
   );
 };
