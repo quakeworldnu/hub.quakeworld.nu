8eccd57ef28961776eed3741cee27f035613ba1c
Viktor Persson <viktor.persson@arcsin.se>
2023-11-13 23:14:38 +0100

    Update list view.

--- src/pages/recent_games/browser/DemoList.tsx
+++ src/pages/recent_games/browser/DemoList.tsx
@@ -6,19 +6,18 @@ import { DownloadButton, PlayButton } from "./Controls.tsx";
 
 export const DemoList = ({ demos }: { demos: Demo[] | null }) => {
   return (
-    <table className="text-left text-sm">
+    <table className="text-left">
       <thead>
-        <tr>
-          <th className="p-2 min-w-[120px]">Timestamp</th>
-          <th className="p-2">Server</th>
+        <tr className="text-slate-300 text-xs">
+          <th className="p-2 min-w-[100px]">Timestamp</th>
           <th className="p-2">Mode</th>
           <th className="p-2">Map</th>
-          <th className="p-2">Score</th>
           <th className="p-2">Title</th>
+          <th className="p-2">Score</th>
           <th className="p-2"></th>
         </tr>
       </thead>
-      <tbody>
+      <tbody className="text-sm">
         {demos?.map((demo) => <ListItem key={demo.id} demo={demo} />)}
       </tbody>
     </table>
@@ -31,17 +30,14 @@ const ListItem = ({ demo }: { demo: Demo }) => {
       <td className="p-2 text-slate-400 text-xs">
         <Timestamp timestamp={demo.timestamp} />
       </td>
-      <td className="p-2 text-slate-400">
-        {demo.source.replace(":28000", "")}
-      </td>
       <td className="p-2 text-slate-400 text-right">{demo.mode}</td>
       <td className="p-2 text-slate-400">{demo.map}</td>
+      <td className="p-2">{demo.title}</td>
       <td className="text-center">
         <ScoreSpoiler
           score={getDemoScores(demo.participants as DemoParticipants)}
         />
       </td>
-      <td className="p-2">{demo.title}</td>
       <td className="p-2 flex items-center space-x-2">
         <PlayButton id={demo.id} />
         <ToggleButton demo={demo} />
@@ -57,6 +53,7 @@ function getDemoScores(participants: DemoParticipants): string {
   let p: number[];
 
   if (teams.length > 0) {
+    teams.sort((a, b) => a.name.localeCompare(b.name));
     p = teams.map((t) => t.frags);
   } else {
     p = players.map((p) => p.frags);
@@ -64,3 +61,37 @@ function getDemoScores(participants: DemoParticipants): string {
 
   return p.join(" : ");
 }
+
+/*
+const Participants = ({ participants }: { participants: DemoParticipants }) => {
+  const { teams, players } = participants;
+
+  if (teams.length > 0) {
+    return teams
+      .map<ReactNode>((team) => (
+        <span>
+          <strong>
+            <QuakeText text={team.name} color={team.name_color} /> (
+          </strong>
+          {team.players
+            .map<ReactNode>((player) => (
+              <span className="opacity-80">
+                <QuakeText text={player.name} color={player.name_color} />
+              </span>
+            ))
+            .reduce((prev, curr) => [prev, ", ", curr])}
+          )
+        </span>
+      ))
+      .reduce((prev, curr) => [prev, " vs ", curr]);
+  } else {
+    return players
+      .map<ReactNode>((player) => (
+        <strong>
+          <QuakeText text={player.name} color={player.name_color} />
+        </strong>
+      ))
+      .reduce((prev, curr) => [prev, " vs ", curr]);
+  }
+};
+*/
