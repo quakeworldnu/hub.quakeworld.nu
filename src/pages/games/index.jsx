1f776f362114e2243bceebfbb0b93fd10fa1b194
Viktor Persson <viktor.persson@arcsin.se>
2023-12-24 22:09:15 +0100

    Limit sentry reporting to production environment.

--- src/pages/games/index.jsx
+++ src/pages/games/index.jsx
@@ -1,22 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
-// import { ConvexProvider, ConvexReactClient } from "convex/react";
+import { Provider } from "react-redux"; // import { ConvexProvider, ConvexReactClient } from "convex/react";
 import store from "@qwhub/store";
 import App from "./App";
 import "@qwhub/styles/index.scss";
 import { DemoProvider } from "@qwhub/pages/games/browser/context";
-import { ClipEditorProvider } from "@qwhub/pages/games/player/clips/context";
-
-// const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL);
+import { ClipEditorProvider } from "@qwhub/pages/games/player/clips/context"; // const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL);
 import * as Sentry from "@sentry/react";
 import { Wasm as WasmIntegration } from "@sentry/wasm";
 
-Sentry.init({
-  dsn: import.meta.env.VITE_SENTRY_DSN,
-  integrations: [new WasmIntegration()],
-  environment: import.meta.env.VITE_SENTRY_ENVIRONMENT,
-});
+if (import.meta.env.PROD) {
+  Sentry.init({
+    dsn: import.meta.env.VITE_SENTRY_DSN,
+    integrations: [new WasmIntegration()],
+    environment: import.meta.env.VITE_SENTRY_ENVIRONMENT,
+  });
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   /*<React.StrictMode>*/
