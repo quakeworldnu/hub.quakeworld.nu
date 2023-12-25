89abc2ab15aab2a71524dd04b1f54e8999cde7b1
Viktor Persson <viktor.persson@arcsin.se>
2023-12-25 15:15:33 +0100

    Update control styles.

--- src/pages/games/player/Controls.tsx
+++ src/pages/games/player/Controls.tsx
@@ -37,18 +37,18 @@ export const Controls = () => {
         <TimeSlider />
       </div>
 
-      <div className="flex w-full sm:w-1/3 sm:space-x-2 items-center sm:my-1">
+      <div className="flex w-full sm:w-1/3 sm:space-x-2 items-center sm:mb-2">
         <Volume />
         <GameClock />
       </div>
 
-      <div className="hidden sm:flex space-x-1 items-center justify-center my-1">
+      <div className="hidden sm:flex space-x-1 items-center justify-center mb-2">
         <SeekToStartButton />
         <PlayToggle />
         <SeekToEndButton />
       </div>
 
-      <div className="hidden sm:flex w-1/3 space-x-2 items-center justify-end my-1 ">
+      <div className="hidden sm:flex w-1/3 space-x-2 items-center justify-end mb-2 ">
         <AutotrackToggle />
         <SlowmotionToggle />
         <SplitscreenToggle />
