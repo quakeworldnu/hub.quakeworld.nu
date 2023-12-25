3e034311018223e9319a23f145f296d4b092e009
Viktor Persson <viktor.persson@arcsin.se>
2023-12-25 15:29:51 +0100

    Update app styles.

--- src/pages/games/App.jsx
+++ src/pages/games/App.jsx
@@ -77,7 +77,7 @@ export const App = () => {
   return (
     <div className="flex flex-col">
       <SiteHeader />
-      <div className="3xl:flex gap-6 my-6">
+      <div className="3xl:flex gap-6 my-4">
         <div className="w-full">
           <div id="AppBody" ref={bodyRef}>
             {demoId && <Player demoId={demoId} />}
