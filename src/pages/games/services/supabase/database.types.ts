4eed1526de3548c743a617978edafc37d489a17c
Viktor Persson <viktor.persson@arcsin.se>
2023-12-30 22:39:27 +0100

    Update supabase types.

--- src/pages/games/services/supabase/database.types.ts
+++ src/pages/games/services/supabase/database.types.ts
@@ -12,6 +12,7 @@ export interface Database {
       demos: {
         Row: {
           duration: number;
+          event_id: number | null;
           filename: string;
           fts: unknown | null;
           id: number;
@@ -19,6 +20,7 @@ export interface Database {
           matchtag: string;
           mode: string;
           participants: Json | null;
+          participants_sha256: string;
           s3_key: string;
           sha256: string;
           source: string;
@@ -27,6 +29,7 @@ export interface Database {
         };
         Insert: {
           duration?: number;
+          event_id?: number | null;
           filename?: string;
           fts?: unknown | null;
           id?: number;
@@ -34,6 +37,7 @@ export interface Database {
           matchtag?: string;
           mode?: string;
           participants?: Json | null;
+          participants_sha256?: string;
           s3_key?: string;
           sha256?: string;
           source?: string;
@@ -42,6 +46,7 @@ export interface Database {
         };
         Update: {
           duration?: number;
+          event_id?: number | null;
           filename?: string;
           fts?: unknown | null;
           id?: number;
@@ -49,11 +54,41 @@ export interface Database {
           matchtag?: string;
           mode?: string;
           participants?: Json | null;
+          participants_sha256?: string;
           s3_key?: string;
           sha256?: string;
           source?: string;
           timestamp?: string | null;
           title?: string;
+        };
+        Relationships: [
+          {
+            foreignKeyName: "demos_event_id_fkey";
+            columns: ["event_id"];
+            isOneToOne: false;
+            referencedRelation: "events";
+            referencedColumns: ["id"];
+          },
+        ];
+      };
+      events: {
+        Row: {
+          date: string | null;
+          id: number;
+          name: string;
+          s3_dirname: string;
+        };
+        Insert: {
+          date?: string | null;
+          id?: number;
+          name?: string;
+          s3_dirname?: string;
+        };
+        Update: {
+          date?: string | null;
+          id?: number;
+          name?: string;
+          s3_dirname?: string;
         };
         Relationships: [];
       };
@@ -159,3 +194,83 @@ export interface Database {
     };
   };
 }
+
+export type Tables<
+  PublicTableNameOrOptions extends
+    | keyof (Database["public"]["Tables"] & Database["public"]["Views"])
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
+        Database[PublicTableNameOrOptions["schema"]]["Views"])
+    : never = never,
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
+      Database[PublicTableNameOrOptions["schema"]]["Views"])[TableName] extends {
+      Row: infer R;
+    }
+    ? R
+    : never
+  : PublicTableNameOrOptions extends keyof (Database["public"]["Tables"] &
+        Database["public"]["Views"])
+    ? (Database["public"]["Tables"] &
+        Database["public"]["Views"])[PublicTableNameOrOptions] extends {
+        Row: infer R;
+      }
+      ? R
+      : never
+    : never;
+
+export type TablesInsert<
+  PublicTableNameOrOptions extends
+    | keyof Database["public"]["Tables"]
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
+    : never = never,
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+      Insert: infer I;
+    }
+    ? I
+    : never
+  : PublicTableNameOrOptions extends keyof Database["public"]["Tables"]
+    ? Database["public"]["Tables"][PublicTableNameOrOptions] extends {
+        Insert: infer I;
+      }
+      ? I
+      : never
+    : never;
+
+export type TablesUpdate<
+  PublicTableNameOrOptions extends
+    | keyof Database["public"]["Tables"]
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
+    : never = never,
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+      Update: infer U;
+    }
+    ? U
+    : never
+  : PublicTableNameOrOptions extends keyof Database["public"]["Tables"]
+    ? Database["public"]["Tables"][PublicTableNameOrOptions] extends {
+        Update: infer U;
+      }
+      ? U
+      : never
+    : never;
+
+export type Enums<
+  PublicEnumNameOrOptions extends
+    | keyof Database["public"]["Enums"]
+    | { schema: keyof Database },
+  EnumName extends PublicEnumNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicEnumNameOrOptions["schema"]]["Enums"]
+    : never = never,
+> = PublicEnumNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicEnumNameOrOptions["schema"]]["Enums"][EnumName]
+  : PublicEnumNameOrOptions extends keyof Database["public"]["Enums"]
+    ? Database["public"]["Enums"][PublicEnumNameOrOptions]
+    : never;
