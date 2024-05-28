93bb46f895ee471e2bbec70104f09d8dcd18e385
Viktor Persson <viktor.persson@arcsin.se>
2024-05-28 10:19:13 +0200

    Delay fetching of streams due to rate limiting.

--- src/servers/ServerStreams.jsx
+++ src/servers/ServerStreams.jsx
@@ -1,11 +1,22 @@
 import { TwitchButton } from "@qwhub/Buttons";
 import { useGetStreamsQuery } from "@qwhub/services/hub/hub";
-import React from "react";
+import React, { useState } from "react";
+import { useTimeout } from "usehooks-ts";
+
+function useDelay(delay) {
+  const [skip, setSkip] = useState(true);
+  useTimeout(() => {
+    setSkip(false);
+  }, delay);
+  return skip;
+}
 
 const ServersStreams = React.memo((props) => {
+  const skip = useDelay(1500);
   const { address } = props;
   const { data: allStreams = [] } = useGetStreamsQuery(null, {
     pollingInterval: 15500,
+    skip,
   });
   const streamsOnServer = allStreams.filter(
     (s) => s.server_address === address,
