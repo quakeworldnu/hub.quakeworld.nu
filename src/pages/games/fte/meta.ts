d2a440d73a48ad84dc69e260f739a4cca1c29e58
Viktor Persson <viktor.persson@arcsin.se>
2024-07-15 00:31:39 +0200

    Bump asset versions.

--- src/pages/games/fte/meta.ts
+++ src/pages/games/fte/meta.ts
@@ -2,5 +2,5 @@ export const DEMO_CONFIG_VERSION = 7;
 export const DEMO_FTE_VERSION = 4;
 export const DEMO_CSADDON_VERSION = 4;
 
-export const QTV_CONFIG_VERSION = 1;
-export const QTV_FTE_VERSION = 2;
+export const QTV_CONFIG_VERSION = 2;
+export const QTV_FTE_VERSION = 3;
