1df495f8b0c1c5c5993d0c47b76387f8e9ba657c
Viktor Persson <viktor.persson@arcsin.se>
2024-07-19 10:36:17 +0200

    Update search params on server change.

--- src/pages/qtv/events.ts
+++ src/pages/qtv/events.ts
@@ -1,5 +1,17 @@
 import { MvdsvServer } from "@qwhub/pages/qtv/types.ts";
 
+function updateSearchParams(address: string) {
+  const url = new URL(window.location.href);
+
+  if (url.searchParams.get("address") === address) {
+    return;
+  }
+
+  url.searchParams.set("address", address);
+  history.pushState(null, "", url);
+}
+
 export function selectServer(server: MvdsvServer) {
+  updateSearchParams(server.address);
   window.dispatchEvent(new CustomEvent("hub.selectServer", { detail: server }));
 }
