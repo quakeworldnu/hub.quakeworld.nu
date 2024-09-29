4935b3f9db65f08e4271895736eaa428fc82bf65
Viktor Persson <viktor.persson@arcsin.se>
2024-09-29 13:56:15 +0200

    Remove convex.

--- src/pages/games/index.jsx
+++ src/pages/games/index.jsx
@@ -1,7 +1,7 @@
 import store from "@qwhub/store";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux"; // import { ConvexProvider, ConvexReactClient } from "convex/react";
+import { Provider } from "react-redux";
 import App from "./App";
 import "@qwhub/styles/index.scss";
 import * as Sentry from "@sentry/react";
@@ -16,9 +16,7 @@ if (import.meta.env.PROD) {
 ReactDOM.createRoot(document.getElementById("root")).render(
   /*<React.StrictMode>*/
   <Provider store={store}>
-    {/*<ConvexProvider client={convex}>*/}
     <App />
-    {/*</ConvexProvider>*/}
   </Provider>,
   /*</React.StrictMode>,*/
 );
