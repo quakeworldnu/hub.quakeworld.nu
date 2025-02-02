021a764a11b85a1528c93391d6aad3cd6644f5a1
Viktor Persson <vikpe@users.noreply.github.com>
2025-02-02 19:26:10 +0100

    Bump asset versions.

--- src/pages/games/fte/meta.ts
+++ src/pages/games/fte/meta.ts
@@ -1,6 +1,6 @@
 export const DEMO_CONFIG_VERSION = 8;
-export const DEMO_FTE_VERSION = 5;
-export const DEMO_CSADDON_VERSION = 6;
+export const DEMO_FTE_VERSION = 6;
+export const DEMO_CSADDON_VERSION = 7;
 
 export const QTV_CONFIG_VERSION = 4;
 export const QTV_FTE_VERSION = 7;
