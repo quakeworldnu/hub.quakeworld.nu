1c367632d8f5e1e2a6284d77b2d7a102a184e4da
Viktor Persson <vikpe@users.noreply.github.com>
2025-08-24 11:00:12 +0200

    update uri encoding for asset fetching

--- src/pages/games/services/cloudfront/cassets.ts
+++ src/pages/games/services/cloudfront/cassets.ts
@@ -2,6 +2,6 @@ export const ASSETS_BASE_URL: string = import.meta.env
   .VITE_ASSETS_CLOUDFRONT_URL;
 
 export function getAssetUrl(path: string): string {
-  const path_ = path.replace("#", "%23").replace("+", "%2B");
+  const path_ = path.replaceAll("#", "%23").replaceAll("+", "%2B");
   return `${ASSETS_BASE_URL}/${path_}`;
 }
